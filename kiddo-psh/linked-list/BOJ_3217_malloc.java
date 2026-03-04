ecdaa334101b8789e74702f2d91bb6429ad717f8
kiddo-psh <kiddo.psh@gmail.com>
2026-03-05 08:46:54 +0900

    Refactor memory management logic in BOJ_3217_malloc

--- kiddo-psh/linked-list/BOJ_3217_malloc.java
+++ kiddo-psh/linked-list/BOJ_3217_malloc.java
@@ -6,13 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 /*
- * 메모리: 38,592 kb
- * 실행시간: 504 ms
+ * 메모리: 36,268 kb
+ * 실행시간: 524 ms
  * 
  * @param Node 메모리 할당된 영역을 start, end로 저장
  * @param list 메모리 주소 순으로 할당한 값들을 순회할 수 있도록 만듦
- * @param map 해당 변수가 할당된 메모리 주소 매핑
- * @param nodeMap 리스트에서 노드를 O(1)의 속도로 제거하기 위한 map 
+ * @param nodeMap 해당 변수가 할당된 메모리 주소 매핑, 리스트에서 노드를 O(1)의 속도로 제거하기 위한 map 
  * 
  * # malloc
  * 1. 리스트를 순회하며 빈 칸이 size 이상인지 확인한다.
@@ -21,17 +20,17 @@ import java.util.Stack;
  * 
  * # free
  * 1. 해당 변수에 메모리가 할당되었는지 확인한다.
- * 2. 할당되어있다면 nodeMap으로 접근하여 list에서 해당 노드를 제거
- * 3. nodeMap에서도 제거
+ * 2. 할당되어있다면 map으로 접근하여 list에서 해당 노드를 제거
+ * 3. map에서 제거
  * 
  * # print
  * 1. 해당 변수에 메모리가 할당되었는지 확인한다.
- * 2. 할당되어있다면 map에서 해당 변수의 value를 0으로 만든다.
+ * 2. 할당되어있다면 map에서 해당 변수의 시작주소 출력.
+ * 3. 아니라면 0 출력 
  * 
  */
-public class BOJ_3217_malloc {
+public class Main {
 	static LinkedList list = new LinkedList();
-	static Map<String, Integer> map = new HashMap<>();
 	static Map<String ,Node> nodeMap = new HashMap<>(); 
 	static final int MAX_MEMORY = 100_000;
 	
@@ -100,21 +99,18 @@ public class BOJ_3217_malloc {
 			nextNode.prev.next = n;
 			nextNode.prev = n;
 			
-			map.put(var, cur);
 			nodeMap.put(var, n);
 			
 			isAllocated = true;
 			break;
 		}
 		if (!isAllocated) {
-			map.put(var, 0);
+			nodeMap.put(var, new Node(0,0));
 		}
 	}
 	
 	static void removeNode(String target) {
-		if (map.get(target)==null || map.get(target)==0) return;
-		
-		map.put(target, 0);
+		if (nodeMap.get(target)==null || nodeMap.get(target).start==0) return;
 		
 		Node n = nodeMap.get(target);
 		n.prev.next = n.next;
@@ -134,20 +130,19 @@ public class BOJ_3217_malloc {
 			// 1. free
 			if (command.startsWith("free(")) {
 				String target = getTarget(command);
-				if (map.get(target)==null || map.get(target)==0) continue;
+				if (nodeMap.get(target)==null || nodeMap.get(target).start==0) continue;
 				
 				removeNode(target);
-				map.put(target, 0);
 			}
 			// 2. print
 			else if (command.startsWith("print(")) {
 				String target = getTarget(command);
 				
-				if (map.get(target)==null || map.get(target)==0) {
+				if (nodeMap.get(target)==null || nodeMap.get(target).start==0) {
 					output.append(0).append("\n");
 					continue;
 				} else {
-					output.append(map.get(target)).append("\n");
+					output.append(nodeMap.get(target).start).append("\n");
 				}
 			}
 			// 3. malloc
